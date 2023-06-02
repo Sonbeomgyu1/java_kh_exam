1120caf067034ba6afc02b5ceecd14c2f2af0c87
Sonbeomgyu1 <sjk6285@naver.com>
2023-06-02 12:29:57 +0900

    book

---
+++ Test11/src/BookTest/BookArray.java
@@ -0,0 +1,15 @@
+package BookTest;
+
+public class BookArray {
+
+	public static void main(String[] args) {
+		
+		
+		Book[] library = new Book[5];
+		
+		for(int i1 = 0 ; i1 < library.length; i1 ++) {
+			System.out.println(library[i1]);
+		}
+	}
+
+}
